c34b1914712fb9ff0b93d17cecc85b1a37448bad
Kylee8126 <kylee8126@gmail.com>
2014-09-25 09:21:49 -0600

    first commit for chatbot made on 9/25/14

---
+++ src/chatbot/model/Chatbot.java
@@ -0,0 +1,78 @@
+package chatbot.model;
+
+public class Chatbot
+{
+	private String name;
+	private int numberOfChats;
+
+	/**
+	 * Creates a Chatbot object with a specified name Initializes the total
+	 * chats 0.
+	 * 
+	 * @param name
+	 *            The name of the chatbot.
+	 */
+	public Chatbot(String name)
+	{
+		this.name = name;
+		numberOfChats = 0;
+	}
+
+	/**
+	 * Gets Chatbot name for user.
+	 * 
+	 * @return
+	 */
+	public String getName()
+	{
+		return this.name;
+	}
+
+	/**
+	 * Gets percise number of chats or messages for user.
+	 * 
+	 * @return
+	 */
+	public int getNumberOfChats()
+	{
+		return numberOfChats;
+	}
+
+	/**
+	 * Gets this SetName for user.
+	 * 
+	 * @param name
+	 */
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	 * Increments the number of chats or messages.
+	 */
+	public void incrementChats()
+	{
+		numberOfChats++;
+	}
+
+	/**
+	 * Checks if user is ready to quit by their input.
+	 * 
+	 * @param input
+	 * @return
+	 */
+	public boolean quitChecker(String input)
+	{
+		boolean okToQuit = false;
+
+		if (input.equals("Goodbye"))
+		{
+			okToQuit = true;
+		}
+
+		return okToQuit;
+
+	}
+
+}
