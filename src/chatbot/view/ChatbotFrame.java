e5f83944bfa4e14e466c81815a2f0810a6851793
Kylee8126 <kylee8126@gmail.com>
2014-10-20 09:53:01 -0600

    Added documentation.

--- src/chatbot/view/ChatbotFrame.java
+++ src/chatbot/view/ChatbotFrame.java
@@ -6,20 +6,30 @@ import chatbot.controller.ChatbotAppController;
 
 public class ChatbotFrame extends JFrame
 {
+    /**
+     * Variable for base panel.
+     */
 	private ChatbotPanel basePanel;
-	
+	/**
+	 * The GUI frame constructor.
+	 * @param baseController
+	 */
 	public ChatbotFrame(ChatbotAppController baseController)
 	{
 		basePanel= new ChatbotPanel(baseController);
 		setupFrame();
 	}
 	
-	 
+	 /**
+	  * This sets up the settings for the frame location. 
+	  */
 	private void setupFrame()
 	{
 		this.setContentPane(basePanel);
+		this.setLocation(67, 67);
 		this.setSize(400, 400);
 		this.setVisible(true);
+		this.setResizable(false);
 	}
 	
 }
--- src/chatbot/view/ChatbotPanel.java
+++ src/chatbot/view/ChatbotPanel.java
@@ -1,34 +1,78 @@
 package chatbot.view;
 
-import javax.swing.JFrame;
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 
 import chatbot.controller.ChatbotAppController;
 
-public class ChatbotPanel extends JFrame
+/**
+ * This is the the GUI panel.
+ * @author kchr8126
+ *
+ */
+public class ChatbotPanel extends JPanel
 {
-
+	/**
+	 * This is the Button constructor.
+	 */
+	private JButton sampleButton;
+	/**
+	 * This is the Text field constructor.
+	 */
+	private JTextField sampleField;
+	/**
+	 * This is the base spring layout constructor.
+	 */
+	private SpringLayout baseLayout;
+	/**
+	 * The constructor for the base controller.
+	 */
 	private ChatbotAppController baseController;
-	
+	/**
+	 * The controller section for this Panel.
+	 * @param baseController
+	 */
 	public ChatbotPanel(ChatbotAppController baseController)
 	{
 		this.baseController = baseController;
+		sampleButton = new JButton("The red button. DON'T CLICK ME!");
+		sampleField = new JTextField(25);
+
+		baseLayout = new SpringLayout();
 		
 		setupPanel();
 		setupLayout();
 		setupListeners();
-	
 	}
-	
+	/**
+	 *This gives the information to how the window looks.
+	 */
 	private void setupPanel()
 	{
+		this.setBackground(Color.CYAN);
+		this.setLayout(baseLayout);
 		
+		this.add(sampleButton);
+		this.add(sampleField);
 	}
-	
+	/**
+	 * This gives the information as to where the button is. Also the junk yard.
+	 */
 	private void setupLayout()
 	{
-		
+		baseLayout.putConstraint(SpringLayout.NORTH, sampleField, 49, SpringLayout.SOUTH, sampleButton);
+		baseLayout.putConstraint(SpringLayout.NORTH, sampleButton, 10, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, sampleButton, 26, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, sampleButton, 33, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, sampleField, 98, SpringLayout.WEST, this);
 	}
-	
+	/**
+	 * This is the section that sets up the listeners.
+	 */
 	private void setupListeners()
 	{
 		
